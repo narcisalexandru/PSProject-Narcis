5b40401e68707693340d2e425695b1c8e63074c4
Cazacu Narcis <cazacu.narcis02@gmail.com>
2025-01-15 11:36:40 +0200

    add carousel

--- src/components/HeaderCarousel.vue
+++ src/components/HeaderCarousel.vue
@@ -1,22 +1,25 @@
 <template>
-    <Swiper
-      :slides-per-view="'auto'"
-      :space-between="30"
-      :navigation="true"
-      :scrollbar="{
-        hide: false,
-        draggable: true,
-      }"
-      :modules="[Navigation, Scrollbar]"
-      class="hero-swiper__container swiper-scrollbar bg-white"
-    >
-        <SwiperSlide class="block w-10 ml-2">
-            <img class="block bg-center border-round-lg w-full h-full " src="/Jan14-2025-Desktop-HP-1.webp" />
-        </SwiperSlide>
-        <SwiperSlide class="block w-10 mr-2">
-            <img class="block bg-center border-round-lg w-full h-full " src="/Jan14-2025-Desktop-HP-2.webp" />
-        </SwiperSlide>
-    </Swiper>
+    <div class="m-5">
+
+        <Swiper
+        :slides-per-view="'auto'"
+          :space-between="30"
+          :navigation="true"
+          :scrollbar="{
+            hide: false,
+            draggable: true,
+          }"
+          :modules="[Navigation, Scrollbar]"
+          class="hero-swiper__container swiper-scrollbar bg-white"
+        >
+            <SwiperSlide class="block w-10 ml-2">
+                <img class="block bg-center border-round-lg w-full h-full " src="/Jan14-2025-Desktop-HP-1.webp" />
+            </SwiperSlide>
+            <SwiperSlide class="block w-10 mr-2">
+                <img class="block bg-center border-round-lg w-full h-full " src="/Jan14-2025-Desktop-HP-2.webp" />
+            </SwiperSlide>
+        </Swiper>
+    </div>
 </template>
   
 <script setup>
@@ -25,6 +28,10 @@ import { SwiperSlide } from 'swiper/vue';
 </script>
 
 <style>
+.hero-swiper__container:hover .swiper-button-next,
+.hero-swiper__container:hover .swiper-button-prev  {
+    display: flex;
+}
 .hero-swiper__container .swiper-scrollbar {
     display: block;
     position: relative;
@@ -36,5 +43,24 @@ import { SwiperSlide } from 'swiper/vue';
     margin: auto;
     margin-top: 3rem !important;
     transition: 0.35s;
+    cursor: pointer;
+}
+.swiper-button-next, .swiper-button-prev {
+    display: none;
+    background-color: #0e0f11;
+    border-radius: 50%;
+    background-image: unset;
+    height: 48px;
+    width: 48px;
+}
+.swiper-button-next::after, .swiper-button-prev::after {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px !important;
+    color: white;
+}
+.swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
+    display: none !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
+import 'swiper/css/bundle';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
