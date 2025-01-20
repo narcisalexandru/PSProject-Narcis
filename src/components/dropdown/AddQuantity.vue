bbcc383df5966eed4fb12bbbde0a260d4b666f3e
Cazacu Narcis <cazacu.narcis02@gmail.com>
2025-01-20 15:52:29 +0200

    done :  dropdrown keep the value in store, and put it in another fields

---
+++ src/components/dropdown/AddQuantity.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card flex justify-content-center align-items-center w-full gap-8 mt-8">
+        <Select v-model="selectedQtyL" :options="quantities" optionLabel="label" placeholder="Select a City" class="w-2" />
+        <Select v-model="selectedQtyR" :options="quantities" optionLabel="label" placeholder="Select a City" class="w-2" />
+        <ModalQuantity />
+    </div>
+</template>
+
+<script setup>
+import { useQuantityStore } from "@/store/quantityStore";
+import { ref, computed } from "vue";
+import ModalQuantity from "./ModalQuantity.vue";
+
+const quantityStore = useQuantityStore();
+
+const quantities = ref([
+  { label: '1 box', value: 1 },
+  { label: '2 boxes', value: 2 },
+  { label: '3 boxes', value: 3 },
+  { label: '4 boxes', value: 4 },
+  { label: '5 boxes', value: 5 },
+  { label: '6 boxes', value: 6 },
+  { label: '7 boxes', value: 7 },
+  { label: '8 boxes', value: 8 },
+  { label: '9 boxes', value: 9 },
+  { label: '10 boxes', value: 10 },
+  { label: '11 boxes', value: 11 },
+  { label: '12 boxes', value: 12 }
+]);
+
+const selectedQtyL = computed({
+  get: () => quantityStore.selectedQtyL,
+  set: (qty) => quantityStore.setSelectedQtyL(qty)
+});
+
+const selectedQtyR = computed({
+  get: () => quantityStore.selectedQtyR,
+  set: (qty) => quantityStore.setSelectedQtyR(qty)
+});
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
 	<h1 class="flex justify-content-center mt-8">Home View</h1>
+	<AddQuantity />
 </template>
 
 <script setup>
+import AddQuantity from '@/components/dropdown/AddQuantity.vue';
+
 
 </script>
 
