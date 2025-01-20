82e0fcd70d00bba073a051e4fb264f19a68902dc
Cazacu Narcis <cazacu.narcis02@gmail.com>
2025-01-20 11:12:53 +0200

    fix swiper question

--- src/components/HeaderCarousel.vue
+++ src/components/HeaderCarousel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="m-5">
+    <div class="m-5 wrapper">
         <Swiper
             :slides-per-view="'auto'"
             :space-between="15"
@@ -79,64 +79,63 @@ import { SwiperSlide } from 'swiper/vue';
 </script>
 
 <style>
-.hero-swiper__container:hover .swiper-button-next,
-.hero-swiper__container:hover .swiper-button-prev  {
-    display: flex;
+.wrapper {
+    .hero-swiper__container:hover .swiper-button-next,
+    .hero-swiper__container:hover .swiper-button-prev  {
+        display: flex;
+    }
+    .hero-swiper__container .swiper-scrollbar {
+        display: block;
+        position: relative;
+        max-width: 50%;
+        height: 6px;
+        left: 0;
+        top: 0;
+        margin: auto;
+        margin-top: 3rem !important;
+        transition: 0.35s;
+        cursor: pointer;
+    }
+    .swiper-button-next, .swiper-button-prev {
+        display: none;
+        background-color: #0e0f11;
+        border-radius: 50%;
+        background-image: unset;
+        height: 48px;
+        width: 48px;
+    }
+    .swiper-button-next::after, .swiper-button-prev::after {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 30px !important;
+        color: white;
+    }
+    .swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
+        display: none !important;
+    }
+    
+    .header-text-wrapper {
+        max-width: 50%;
+        transform: translateY(50%);
+        padding: 20px;
+        transition: 0.50s;
+    }
+    .header-description {
+        line-height: initial;
+        color: #414141;
+    }
+    .header-coupon {
+        color: #414141;
+    }
+    .hero__insurance {
+        background-color: #415FF1;
+        border-radius: 0 8px 0 8px;
+    }
+    
+    .swiper-slide:hover {
+        transform: scale(1.01);
+        transition: 600ms;
+    }
 }
-.hero-swiper__container .swiper-scrollbar {
-    display: block;
-    position: relative;
-    max-width: 50%;
-    height: 6px;
-    left: 0;
-    top: 0;
-    margin: auto;
-    margin-top: 3rem !important;
-    transition: 0.35s;
-    cursor: pointer;
-}
-.swiper-button-next, .swiper-button-prev {
-    display: none;
-    background-color: #0e0f11;
-    border-radius: 50%;
-    background-image: unset;
-    height: 48px;
-    width: 48px;
-}
-.swiper-button-next::after, .swiper-button-prev::after {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 30px !important;
-    color: white;
-}
-.swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
-    display: none !important;
-}
-
-.header-text-wrapper {
-    max-width: 50%;
-    transform: translateY(50%);
-    padding: 20px;
-    transition: 0.50s;
-}
-.header-description {
-    line-height: initial;
-    color: #414141;
-}
-.header-coupon {
-    color: #414141;
-}
-.hero__insurance {
-    background-color: #415FF1;
-    border-radius: 0 8px 0 8px;
-}
-
-/* transform */
-
-.swiper-slide:hover {
-    transform: scale(1.01);
-    transition: 600ms;
-}
-
 </style>
