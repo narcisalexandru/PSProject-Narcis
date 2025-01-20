6e43359e30791610585b2a5275ce088cc9ebdd17
Cazacu Narcis <cazacu.narcis02@gmail.com>
2025-01-20 11:01:51 +0200

    add WhereToStart section

---
+++ src/components/WhereToStart.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="m-8">
+        <div class="grid">
+            <div class="col-5 p-8 flex flex-column justify-content-center">
+                <h1>
+                    Not sure where to start?
+                </h1>
+                <div class="">Choosing the right glasses is an investment in yourself. Our curated guides provide expert advice and helpful tips to make your online shopping experience seamless and enjoyable.</div>
+            </div>
+            <div class="col-7">
+                <div class="flex">
+                    <div class="col-6 my-auto text-sm mx-auto">
+                        <a href="https://www.kits.ca/insurance" target="_blank" rel= "noopener noreferrer">
+                            <div class="glasses-questions__tile m-2 p-4 flex flex-column align-items-center justify-content-center">
+                                <img src="/shield-with-circled-checkmark.svg"/>
+                                <div>How to use your insurance</div>
+                            </div>
+                        </a>
+                        <a href="https://www.kits.ca/face_shape_guide" target="_blank" rel= "noopener noreferrer">
+                            <div class="glasses-questions__tile m-2 p-4 flex flex-column align-items-center justify-content-center">
+                                <img src="/round-glasses-rim.svg"/>
+                                <div>Shop your face shape</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <a href="https://www.kits.ca/pd-tool" target="_blank" rel= "noopener noreferrer">
+                            <div class="glasses-questions__tile m-2 p-4 flex flex-column align-items-center justify-content-center">
+                                <img src="/pd-ruler.svg"/>
+                                <div>Measure your pupillary distance</div>
+                            </div>
+                        </a>
+                        <a href="https://www.kits.ca/how_to_buy_glasses_online" target="_blank" rel= "noopener noreferrer">
+                            <div class="glasses-questions__tile m-2 p-4 flex flex-column align-items-center justify-content-center">
+                                <img src="/cart-in-monitor-and-phone.svg"/>
+                                <div>How to buy glasses online</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.glasses-questions__tile {
+    background-color: #F5F6F7;
+    border-radius: 8px;
+    text-align: center;
+    color: #0E0F11 !important;
+    text-decoration: underline;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
-	<SearchBar />
-    <h1 class="p-8 flex justify-content-center align-items-center">Selecteaza un mod in care vrei sa vezi pagina</h1>
+	<WhereToStart />
 </template>
 
 <script setup>
@@ -8,45 +7,5 @@
 </script>
 
 <style>
-/* h1 {
-    animation-name: textShake;
-    animation-duration: 1s;
-    animation-iteration-count:calc(5/1);
-} */
 
-/* @keyframes textShake {
-0% {
-	transform: translate(0);
-}
-10% {
-	transform: translate(-25px, -25px);
-}
-250% {
-	transform: translate(25px, -25px);
-}
-30% {
-	transform: translate(-25px, 25px);
-}
-40% {
-	transform: translate(25px, 25px);
-}
-250% {
-	transform: translate(-25px, -25px);
-}
-60% {
-	transform: translate(25px, -25px);
-}
-70% {
-	transform: translate(-25px, 25px);
-}
-80% {
-	transform: translate(-25px, -25px);
-}
-90% {
-	transform: translate(25px, -25px);
-}
-100% {
-	transform: translate(0);
-}
-} */
 </style>
